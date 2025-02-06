ea0d9b4c6ad9b44bb5302a531f7b5b5e7e2bc1c7
David Joseph Candelaria <36684472+Dc-devRex@users.noreply.github.com>
2025-02-06 16:45:08 -0700

    feature: request dog ids in first request and gather dog details based on id in second request

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -11,21 +11,95 @@ interface Dog {
 
 const Feed = () => {
     const [dogData, setDogData] = useState<Dog[] | null>(null);
-    const [loadingResults, setloadingResults] = useState(true);
+    const [loadingResults, setLoadingResults] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        const fetchDogData = async () => {
+        const fetchDogIds = async () => {
             try {
+                const dogIdResponse = await fetch(
+                    "https://frontend-take-home-service.fetch.com/dogs/search",
+                    {
+                        method: 'GET',
+                        credentials: 'include',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                );
 
-            } catch {
+                if (!dogIdResponse.ok) {
+                    throw new Error(`HTTP error! Status: ${dogIdResponse.status}`);
+                }
 
+                const dogIdResult = await dogIdResponse.json();
+                const dogIds: string[] = dogIdResult.resultIds
+
+                if (dogIds.length > 0) {
+                    fetchDogDetails(dogIds);
+                } else {
+                    setLoadingResults(false);
+                }
+          
+            } catch (err: any) {
+                setError(err.message);
+                setLoadingResults(false);
             }
         }
-    });
+
+        const fetchDogDetails = async (dogIds: string[]) => {
+            try {
+                const dogDetailsResponse = await fetch(
+                    "https://frontend-take-home-service.fetch.com/dogs",
+                    {
+                        method: 'POST',
+                        credentials: 'include',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(dogIds),
+                    }
+                );
+
+                if (!dogDetailsResponse.ok) {
+                    throw new Error(`HTTP error! Status: ${dogDetailsResponse.status}`);
+                }
+
+                const dogDetails: Dog[] = await dogDetailsResponse.json();
+                setDogData(dogDetails);
+
+            } catch(err: any) {
+                setError(err.message);
+            } finally {
+                setLoadingResults(false);
+            }
+        }
+
+        fetchDogIds();
+    }, []);
+
+    if (loadingResults) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
 
     return(
-        <h1>Welcome!</h1>
+        <div>
+            <h1>Your Potential Pawtners</h1>
+            {dogData && dogData.length > 0 ? (              
+                <div>
+                { dogData.map((dog) => (
+                    <div key={dog.id}>
+                    <img src={dog.img} alt={dog.name} />
+                    <h2>{dog.name}</h2>
+                    <p>Age: {dog.age}</p>
+                    <p>Breed: {dog.breed}</p>
+                    <p>Zip Code: {dog.zip_code}</p>
+                    </div>
+                )) }
+                </div>
+            ) : (
+                <p>No dogs found</p>
+        )}
+        </div>
     );
 };
 
