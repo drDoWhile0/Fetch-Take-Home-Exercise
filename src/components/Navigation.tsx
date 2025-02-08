f552efa4b175e959b2dffe2b584d602df195ebe9
David Joseph Candelaria <36684472+Dc-devRex@users.noreply.github.com>
2025-02-08 13:45:28 -0700

    feature: create new Navigation component - used as a sidebar component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navigation = () => {
+    return(
+        <div>
+            <a>Feed</a>
+            <a>Favorites</a>
+            <a>Account</a>
+            <a>Logout</a>
+        </div>
+    );
+}
+
+export default Navigation;
