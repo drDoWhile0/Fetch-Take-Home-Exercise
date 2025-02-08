f250992742097bc7733e089b63ac7254be98831f
David Joseph Candelaria <36684472+Dc-devRex@users.noreply.github.com>
2025-02-07 21:23:51 -0700

    feature: render new route for the favorites component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Feed from './components/Feed';
+import Favorites from './components/Favorites';
 import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={ <Feed /> }></Route>
-        <Route path='/login' element= { <Login /> }></Route>
+        <Route path="/" element={<Feed />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
