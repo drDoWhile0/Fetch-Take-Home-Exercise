25c8d990f23426f745fc94292ff0c37a53b1ada2
David Joseph Candelaria <36684472+Dc-devRex@users.noreply.github.com>
2025-02-08 18:30:21 -0700

    feature: connect the navigational links to the routes configured in Router

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from '@mui/material';
 import PetsIcon from '@mui/icons-material/Pets';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Navigation = () => {
     return(
@@ -11,30 +12,33 @@ const Navigation = () => {
                     <PetsIcon className='login-component__heading-icon-container__paw-icon' sx={{ color: '#FBA919', height: '30px', width: '30px' }} />
                 </div>
                 <div className='navigation-container__elements-links'>
-                    <a>Feed</a>
-                    <a>Favorites</a>
-                    <a>Account</a>
+                    <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+                        <a>Feed</a>
+                    </Link>
+                    <Link to="/favorites" style={{ textDecoration: 'none', color: 'white' }}>
+                        <a>Favorites</a>
+                    </Link>
                 </div>
             </div>
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    className="login-component__login-card__button"
-                    sx={{ 
-                        mt: 3, 
-                        mb: 2,
-                        backgroundColor: "#FFA900",
-                        color: 'black',
-                        fontFamily: '"Bree Serif", serif',
-                        "&:hover": {
-                            backgroundColor: "#FF5733",
-                            color: "white",
-                        },
-                    }}
-                >
-                    Log Out
-                </Button>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                className="login-component__login-card__button"
+                sx={{ 
+                    mt: 3, 
+                    mb: 2,
+                    backgroundColor: "#FFA900",
+                    color: 'black',
+                    fontFamily: '"Bree Serif", serif',
+                    "&:hover": {
+                        backgroundColor: "#FF5733",
+                        color: "white",
+                    },
+                }}
+            >
+                Log Out
+            </Button>
         </>
     );
 }
